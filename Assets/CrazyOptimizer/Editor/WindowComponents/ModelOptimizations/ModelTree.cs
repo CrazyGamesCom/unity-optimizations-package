10de7aa0d2f3bd9dd635c29f0253ca93048f23e8
Zoltán Gubics <gubicsz@gmail.com>
2022-09-06 20:57:24 +0200

    Add Models tab to the Optimizer editor window

---
+++ Assets/CrazyOptimizer/Editor/WindowComponents/ModelOptimizations/ModelTree.cs
@@ -0,0 +1,143 @@
+using CrazyGames.TreeLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+
+namespace CrazyGames.WindowComponents.ModelOptimizations
+{
+    class ModelTree : TreeViewWithTreeModel<ModelTreeItem>
+    {
+        public ModelTree(TreeViewState treeViewState, MultiColumnHeader multiColumnHeader, TreeModel<ModelTreeItem> model)
+            : base(treeViewState, multiColumnHeader, model)
+        {
+            showBorder = true;
+            showAlternatingRowBackgrounds = true;
+            multiColumnHeader.sortingChanged += OnSortingChanged;
+            Reload();
+        }
+
+        void SortIfNeeded(TreeViewItem root, IList<TreeViewItem> rows)
+        {
+            if (rows.Count <= 1)
+                return;
+
+            if (multiColumnHeader.sortedColumnIndex == -1)
+            {
+                return; // No column to sort for (just use the order the data are in)
+            }
+
+            var sortedColumns = multiColumnHeader.state.sortedColumns;
+
+            if (sortedColumns.Length == 0)
+                return;
+
+            var items = rootItem.children.Cast<TreeViewItem<ModelTreeItem>>().OrderBy(i => i.data.modelName);
+            var sortedColumnIndex = sortedColumns[0];
+            var ascending = multiColumnHeader.IsSortedAscending(sortedColumnIndex);
+
+            switch (sortedColumnIndex)
+            {
+                case 0:
+                    items = items.Order(i => i.data.modelName, ascending);
+                    break;
+                case 1:
+                    items = items.Order(i => i.data.isReadable, ascending);
+                    break;
+                case 2:
+                    items = items.Order(i => i.data.meshCompression, ascending);
+                    break;
+                case 3:
+                    items = items.Order(i => i.data.animationCompression, ascending);
+                    break;
+            }
+
+            rootItem.children = items.Cast<TreeViewItem>().ToList();
+            TreeToList(root, rows);
+            Repaint();
+        }
+
+        public static void TreeToList(TreeViewItem root, IList<TreeViewItem> result)
+        {
+            if (root == null)
+                throw new NullReferenceException("root");
+            if (result == null)
+                throw new NullReferenceException("result");
+
+            result.Clear();
+
+            if (root.children == null)
+                return;
+
+            Stack<TreeViewItem> stack = new Stack<TreeViewItem>();
+
+            for (int i = root.children.Count - 1; i >= 0; i--)
+                stack.Push(root.children[i]);
+
+            while (stack.Count > 0)
+            {
+                TreeViewItem current = stack.Pop();
+                result.Add(current);
+
+                if (current.hasChildren && current.children[0] != null)
+                {
+                    for (int i = current.children.Count - 1; i >= 0; i--)
+                    {
+                        stack.Push(current.children[i]);
+                    }
+                }
+            }
+        }
+
+        void OnSortingChanged(MultiColumnHeader multiColumnHeader)
+        {
+            SortIfNeeded(rootItem, GetRows());
+        }
+
+        protected override IList<TreeViewItem> BuildRows(TreeViewItem root)
+        {
+            var rows = base.BuildRows(root);
+            SortIfNeeded(root, rows);
+            return rows;
+        }
+
+        protected override void RowGUI(RowGUIArgs args)
+        {
+            var item = (TreeViewItem<ModelTreeItem>)args.item;
+
+            for (int i = 0; i < args.GetNumVisibleColumns(); ++i)
+            {
+                CellGUI(args.GetCellRect(i), item, args.GetColumn(i), ref args);
+            }
+        }
+
+        private void CellGUI(Rect cellRect, TreeViewItem<ModelTreeItem> item, int column, ref RowGUIArgs args)
+        {
+            CenterRectUsingSingleLineHeight(ref cellRect);
+            switch (column)
+            {
+                case 0:
+                    GUI.Label(cellRect, item.data.modelName);
+                    break;
+                case 1:
+                    GUI.Label(cellRect, item.data.isReadable ? "yes" : "no");
+                    break;
+                case 2:
+                    GUI.Label(cellRect, item.data.meshCompression);
+                    break;
+                case 3:
+                    GUI.Label(cellRect, item.data.animationCompression);
+                    break;
+            }
+        }
+
+        protected override void SelectionChanged(IList<int> selectedIds)
+        {
+            base.SelectionChanged(selectedIds);
+            var item = treeModel.Find(selectedIds.First());
+            Selection.activeObject = AssetDatabase.LoadMainAssetAtPath(item.modelPath);
+        }
+    }
+}
---
+++ Assets/CrazyOptimizer/Editor/WindowComponents/ModelOptimizations/ModelTreeItem.cs
@@ -0,0 +1,62 @@
+using CrazyGames.TreeLib;
+using System;
+using System.IO;
+using UnityEditor;
+
+namespace CrazyGames.WindowComponents.ModelOptimizations
+{
+    public class ModelTreeItem : TreeElement
+    {
+        public readonly string modelPath;
+        public readonly string modelName;
+        public readonly bool isReadable;
+        public readonly string meshCompression; // Off, Low, Medium, High (values in ModelImporterMeshCompression)
+        public readonly string animationCompression; // Off, KeyframeReduction, KeyframeReductionAndCompression, Optimal (values in ModelImporterAnimationCompression)
+
+        public ModelTreeItem(string name, int depth, int id, string modelPath, ModelImporter modelImporter) : base(name, depth, id)
+        {
+            if (depth == -1)
+                return;
+
+            this.modelPath = modelPath;
+            modelName = Path.GetFileName(modelPath);
+            isReadable = modelImporter.isReadable;
+
+            switch (modelImporter.meshCompression)
+            {
+                case ModelImporterMeshCompression.Off:
+                    meshCompression = "Off";
+                    break;
+                case ModelImporterMeshCompression.Low:
+                    meshCompression = "Low";
+                    break;
+                case ModelImporterMeshCompression.Medium:
+                    meshCompression = "Medium";
+                    break;
+                case ModelImporterMeshCompression.High:
+                    meshCompression = "High";
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            switch (modelImporter.animationCompression)
+            {
+                case ModelImporterAnimationCompression.Off:
+                    animationCompression = "Off";
+                    break;
+                case ModelImporterAnimationCompression.KeyframeReduction:
+                    animationCompression = "KeyframeReduction";
+                    break;
+                case ModelImporterAnimationCompression.KeyframeReductionAndCompression:
+                    animationCompression = "KeyframeReductionAndCompression";
+                    break;
+                case ModelImporterAnimationCompression.Optimal:
+                    animationCompression = "Optimal";
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
